96acc7b2850182c7e4837d6abf670f314776ae0d
aitor <aitor@doofinder.com>
2023-08-30 17:33:02 +0200

    Change logger format

--- config/config.exs
+++ config/config.exs
@@ -42,7 +42,7 @@ config :esbuild,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format: "$date $time [$level] [$metadata] \t $message\r\n",
   metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
