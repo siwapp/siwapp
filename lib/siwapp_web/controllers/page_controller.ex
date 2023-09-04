ed1f1ff80dbf9790a4c5ce1e15d1e4b1ee35a722
aitor <aitor@doofinder.com>
2023-09-04 17:36:24 +0200

    Added invoice-number to csv file when downloaded

--- lib/siwapp_web/controllers/page_controller.ex
+++ lib/siwapp_web/controllers/page_controller.ex
@@ -56,6 +56,7 @@ defmodule SiwappWeb.PageController do
       |> Kernel.--([:meta_attributes])
       |> Kernel.++([:inserted_at, :updated_at])
       |> maybe_add_series_code_key(params["view"])
+      |> maybe_add_invoice_number_key(params["view"])
       |> maybe_delete_key(params["view"])
       |> maybe_add_meta_attributes_key(params)
 
@@ -111,6 +112,10 @@ defmodule SiwappWeb.PageController do
   defp maybe_add_series_code_key(keys, "invoice"), do: keys ++ [:series_code]
   defp maybe_add_series_code_key(keys, _else), do: keys
 
+  @spec maybe_add_invoice_number_key([atom], binary) :: [atom]
+  defp maybe_add_invoice_number_key(keys, "invoice"), do: keys ++ [:invoice_number]
+  defp maybe_add_invoice_number_key(keys, _else), do: keys
+
   @spec maybe_add_meta_attributes_key([atom], map) :: [atom]
   defp maybe_add_meta_attributes_key(keys, %{"csv_meta_attributes" => meta_attributes_params}) do
     keys ++
@@ -159,6 +164,7 @@ defmodule SiwappWeb.PageController do
   defp prepare_values(struct, fields, params) do
     struct
     |> maybe_add_series_code()
+    |> maybe_add_invoice_number()
     |> maybe_add_meta_attributes(params)
     |> sort_values(fields)
   end
@@ -168,6 +174,12 @@ defmodule SiwappWeb.PageController do
     do: Map.put(invoice, :series_code, code)
 
   defp maybe_add_series_code(other), do: other
+
+  @spec maybe_add_invoice_number(Ecto.Queryable.t()) :: map
+  defp maybe_add_invoice_number(%Invoice{series: %{code: code}, number: number} = invoice),
+    do: Map.put(invoice, :invoice_number, "#{code}-#{number}")
+
+  defp maybe_add_invoice_number(other), do: other
 
   @spec maybe_add_meta_attributes(map, map) :: map()
   defp maybe_add_meta_attributes(%{meta_attributes: meta_attributes} = record, %{
