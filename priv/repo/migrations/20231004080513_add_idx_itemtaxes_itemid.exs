a707cc654e69e51ffe51a2ce09d8f0495844fd37
Jordi Masip <jordi@masip.cat>
2023-10-11 16:37:06 +0200

    Update priv/repo/migrations/20231004080513_add_idx_itemtaxes_itemid.exs
    Co-authored-by: Aitor <93147185+Aitor-Corrales@users.noreply.github.com>

--- priv/repo/migrations/20231004080513_add_idx_itemtaxes_itemid.exs
+++ priv/repo/migrations/20231004080513_add_idx_itemtaxes_itemid.exs
@@ -1,7 +1,5 @@
 defmodule Siwapp.Repo.Migrations.AddIdxItemTaxesItemId do
   use Ecto.Migration
 
-  def up do
-    execute "CREATE INDEX ON items_taxes(item_id);"
-  end
+  def up, do: execute "CREATE INDEX ON items_taxes(item_id);"
 end
