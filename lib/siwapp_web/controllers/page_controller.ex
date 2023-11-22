cdcb4ae81653dde01fe9db41d9719f3e8a8878d0
srteppei <j.hernanz@doofinder.com>
2023-11-22 16:43:36 +0100

    Format

--- lib/siwapp_web/controllers/page_controller.ex
+++ lib/siwapp_web/controllers/page_controller.ex
@@ -14,19 +14,21 @@ defmodule SiwappWeb.PageController do
 
   @spec show_invoice(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show_invoice(conn, %{"id" => id} = params) do
-      invoice = id
+    invoice =
+      id
       |> String.to_integer()
       |> Invoices.get()
 
-      case invoice do
-        {:ok, invoice} ->
-          conn
-          |> assign(:invoice, invoice)
-          |> assign(:url_query_string, Map.delete(params, "id"))
-          |> render("show_invoice.html")
-        nil ->
-          render_not_found(conn)
-      end
+    case invoice do
+      nil ->
+        render_not_found(conn)
+
+      invoice ->
+        conn
+        |> assign(:invoice, invoice)
+        |> assign(:url_query_string, Map.delete(params, "id"))
+        |> render("show_invoice.html")
+    end
   end
 
   @spec download(Plug.Conn.t(), map) :: Plug.Conn.t()
