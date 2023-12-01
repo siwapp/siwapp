cba493a46eb83c077a1063f498fc8c9d60310111
aitor <aitor@doofinder.com>
2023-12-01 13:40:47 +0100

    Fixed invoices' search by customer name

--- lib/siwapp/query.ex
+++ lib/siwapp/query.ex
@@ -17,7 +17,7 @@ defmodule Siwapp.Query do
   """
   @spec search_in_string(Ecto.Queryable.t(), atom, binary) :: Ecto.Query.t()
   def search_in_string(query, field, string_search_value) do
-    where(query, [q], ilike(field(q, ^field), ^string_search_value))
+    where(query, [q], ilike(field(q, ^field), ^"%#{string_search_value}%"))
   end
 
   @spec not_deleted(Ecto.Queryable.t()) :: Ecto.Query.t()
