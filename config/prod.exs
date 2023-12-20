ca1b7c3493f49376adfd9805c20286e752c31fc7
aitor <aitor@doofinder.com>
2023-12-20 15:58:19 +0100

    Create sentry filters

--- config/prod.exs
+++ config/prod.exs
@@ -39,6 +39,7 @@ config :sentry,
   dsn: System.get_env("SENTRY_DSN"),
   environment_name: :prod,
   enable_source_code_context: true,
+  filter: Siwapp.Sentry.EventFilter,
   root_source_code_path: File.cwd!(),
   tags: %{
     env: "production"
---
+++ lib/siwapp/sentry.ex
@@ -0,0 +1,17 @@
+defmodule Siwapp.Sentry do
+  @moduledoc """
+  Module for configuring Sentry
+  """
+
+  defmodule EventFilter do
+    @moduledoc """
+    Module for filtering events sending to Sentry
+    """
+
+    @behaviour Sentry.EventFilter
+
+    @spec exclude_exception?(struct, atom) :: boolean
+    def exclude_exception?(%Siwapp.Error.NotFoundError{}, _), do: true
+    def exclude_exception?(_exception, _source), do: false
+  end
+end
