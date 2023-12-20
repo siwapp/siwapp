76050d35dfbe6c34e72c1b81e518d79e98982ec9
aitor <aitor@doofinder.com>
2023-12-20 11:52:45 +0100

    Solved API issue

--- lib/siwapp_web/views/error_view.ex
+++ lib/siwapp_web/views/error_view.ex
@@ -11,4 +11,9 @@ defmodule SiwappWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
+
+  # This pattern match is to make sure that the api calls
+  # receiving 404 do receive this text instead of the
+  # current template
+  def render("404.html", %{conn: %{path_info: ["api" | _]}}), do: "Not Found"
 end
