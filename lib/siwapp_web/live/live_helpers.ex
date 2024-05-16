bb4724a5449cdb4fe2f470d3edd6e7ac2053c71e
Enrique Martinez <enrique@doofinder.com>
2024-05-16 09:26:39 +0200

    live_title

--- lib/siwapp_web/live/live_helpers.ex
+++ lib/siwapp_web/live/live_helpers.ex
@@ -1,6 +1,5 @@
 defmodule SiwappWeb.LiveHelpers do
   @moduledoc false
-  import Phoenix.LiveView.Helpers
 
   @spec type_of_period(binary, integer) :: binary
   def type_of_period(period_type, period) do
