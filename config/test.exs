0557afe3c470f2f1e878d6aef739883bfafc0c8f
Enrique Martinez <enrique@doofinder.com>
2024-06-08 19:04:39 +0200

    changed hashing algorithm

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,8 @@ config :siwapp,
   env: :test
 
 # Only in tests, remove the complexity from the password hashing algorithm
-config :bcrypt_elixir, :log_rounds, 1
+config :pbkdf2_elixir,
+  rounds: 1
 
 # Configure your database
 #
--- lib/siwapp/accounts/user.ex
+++ lib/siwapp/accounts/user.ex
@@ -83,9 +83,9 @@ defmodule Siwapp.Accounts.User do
 
     if hash_password? && password && changeset.valid? do
       changeset
-      # If using Bcrypt, then further validate it is at most 72 bytes long
+      # If using Pbkdf2, then further validate it is at most 72 bytes long
       |> validate_length(:password, max: 72, count: :bytes)
-      |> put_change(:hashed_password, Bcrypt.hash_pwd_salt(password))
+      |> put_change(:hashed_password, Pbkdf2.hash_pwd_salt(password))
       |> delete_change(:password)
     else
       changeset
@@ -142,16 +142,16 @@ defmodule Siwapp.Accounts.User do
   Verifies the password.
 
   If there is no user or the user doesn't have a password, we call
-  `Bcrypt.no_user_verify/0` to avoid timing attacks.
+  `Pbkdf2.no_user_verify/0` to avoid timing attacks.
   """
   @spec valid_password?(t(), binary) :: boolean()
   def valid_password?(%Siwapp.Accounts.User{hashed_password: hashed_password}, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
-    Bcrypt.verify_pass(password, hashed_password)
+    Pbkdf2.verify_pass(password, hashed_password)
   end
 
   def valid_password?(_, _) do
-    Bcrypt.no_user_verify()
+    Pbkdf2.no_user_verify()
     false
   end
 
