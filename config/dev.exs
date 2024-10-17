80e054fbfad33ad561d47e0271ef8892a319d2ef
Moisès Macià <moises@doofinder.com>
2024-10-17 15:26:58 +0200

    fix docker defaults

--- config/dev.exs
+++ config/dev.exs
@@ -10,7 +10,7 @@ config :siwapp, Siwapp.Repo,
   password: System.get_env("DB_PWD") || "postgres",
   database: System.get_env("DB_NAME") || "siwapp_dev",
   hostname: System.get_env("DB_HOST") || "localhost",
-  port: System.get_env("DB_PORT") || "5432",
+  port: System.get_env("DB_PORT") || 5432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 mix deps.get
+mix deps.clean --unused
+mix compile
+mix ecto.setup
 
 exec "$@"
