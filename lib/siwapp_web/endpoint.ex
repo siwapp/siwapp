557e1f58a31d499db101f6ed61b237a26a0eb4cd
Nayel Velasco <18420051+nvelasco@users.noreply.github.com>
2026-01-22 14:32:03 +0100

    add: custom plug logger

--- lib/siwapp_web/endpoint.ex
+++ lib/siwapp_web/endpoint.ex
@@ -40,12 +40,14 @@ defmodule SiwappWeb.Endpoint do
     cookie_key: "request_logger"
 
   plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint], log: false
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json, Absinthe.Plug.Parser],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+
+  plug SiwappWeb.Plugs.Logger
 
   plug Sentry.PlugContext
   plug Plug.MethodOverride
